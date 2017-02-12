3792825836613e3a96cb79569f31f7c23edb58a4
Allan Targino <altargin@microsoft.com>
2017-02-12 12:02:29 -0200

    documenting Arduino file

--- Arduino/DisplayLED/DisplayLED.ino
+++ Arduino/DisplayLED/DisplayLED.ino
@@ -1,45 +1,56 @@
+//Biblioteca padrão de LCD (16x2)
 #include <LiquidCrystal.h>
 
-String inputString = "";         // a string to hold incoming data
-boolean stringComplete = false;  // whether the string is complete
-int led = 7;
+//String para guardar os dados que chegam pela Serial
+String inputString = "";
+
+//Controle de recebimento
+boolean stringComplete = false;
 
 //Define os pinos que serão utilizados para ligação ao display
 LiquidCrystal lcd(12, 11, 5, 4, 3, 2);
  
-
+//Rotina inicial
 void setup() {
-  // initialize serial:
+  //Inicializa a Serial
   Serial.begin(9600);
 
-  pinMode(led, OUTPUT);
-  digitalWrite(led, HIGH);
-  // reserve 200 bytes for the inputString:
+  //Pré-aloca 200 bytes para inputString
   inputString.reserve(200);
 
   //Define o número de colunas e linhas do LCD
   lcd.begin(16, 2);
 }
 
+//Rotina de repetição
 void loop() {
+  //Apenas mostra o texto ao receber o caracter '\n' 
   if (stringComplete) {
+	//Imprime o texto em modo de depuração
     Serial.println(inputString);
     
+	//Limpa mensagens anteriores
     lcd.clear();
     lcd.setCursor(0, 0);
     lcd.print("Texto:");
     lcd.setCursor(0, 1);
+	//Escreve a mensagem no LCD
     lcd.print(inputString);
 
-    // clear the string:
+    // Limpa a string:
     inputString = "";
     stringComplete = false;
   }
 }
 
+//Rotina de interrupção da Serial
 void serialEvent() {
+  //Enquanto houverem caracteres a leitura ocorre
   while (Serial.available()) {
+	//Guarda o caracter recebido
     char inChar = (char)Serial.read();
+	
+	//Apenas sinaliza o fim da recepeção ao receber o caracter '\n' 
     if (inChar == '\n') {
       stringComplete = true;
     }else{
@@ -47,5 +58,3 @@ void serialEvent() {
     }
   }
 }
-
-
